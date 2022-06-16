4324455f84e14350e8368e52699043ae3a9b6a87
Sadi-Rahman <sadiakash1309@gmail.com>
2022-06-16 17:30:01 +0600

    Added

---
+++ Theatre_Square_1A.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Theatre_Square_1A{
+
+static Scanner sr = new Scanner(System.in);
+
+public static void main(String[] args) {
+  
+    
+    System.out.println("Enter n: ");
+    double n=sr.nextDouble();
+    System.out.println("Enter m: ");
+    double m=sr.nextDouble();
+    System.out.println("Enter a: ");
+    double a=sr.nextDouble();
+
+    double o=(Math.ceil(n/a)*Math.ceil(m/a));
+    System.out.println(o);
+}
+    
+}
