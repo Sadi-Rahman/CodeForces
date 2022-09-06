f6b3c10dc9cde9ffe9f1307c2522953453af9e90
Replit user <>
2022-09-06 14:05:03 +0000

    Added Stars

---
+++ star.java
@@ -0,0 +1,12 @@
+class star{
+  public static void main (String Args[]){
+    int i,j;
+for(i=1;i<=6;i++){
+  for(j=1;j<=i;j++){
+    System.out.print("*");
+  }
+   System.out.println();
+}
+    
+  }
+}
